7308145a769d78eccd76e8eae3879ac9cc71dea7
Jesse B. Hannah <jesse@jbhannah.net>
2019-03-06 11:52:58 -0700

    Youji protobuf spec

---
+++ api/youji/client.go
@@ -0,0 +1,19 @@
+//go:generate protoc --gogofaster_out=plugins=grpc,paths=source_relative:. -I../protobuf youji.proto
+
+package youji
+
+import (
+	"github.com/issho-ni/issho/internal/pkg/service"
+)
+
+// Client is the client to the Youji gRPC service.
+type Client struct {
+	*service.Client
+	YoujiClient
+}
+
+// NewClient returns a client to the Youji gRPC service.
+func NewClient(e service.Env) *Client {
+	c := service.NewClient(e, "youji", "localhost:8082")
+	return &Client{c, NewYoujiClient(c.ClientConn)}
+}
