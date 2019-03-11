69979ac51c694a61edfd34b2474244876abb04e9
Jesse B. Hannah <jesse@jbhannah.net>
2019-03-11 13:27:42 -0700

    Implement inserting user into MongoDB collection

--- internal/app/ninshou/create_user.go
+++ internal/app/ninshou/create_user.go
@@ -3,8 +3,13 @@ package ninshou
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/issho-ni/issho/api/ninshou"
+
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (s *ninshouServer) CreateUser(ctx context.Context, in *ninshou.NewUser) (*ninshou.User, error) {
@@ -20,5 +25,23 @@ func (s *ninshouServer) CreateUser(ctx context.Context, in *ninshou.NewUser) (*n
 		return nil, errors.New("password can't be empty")
 	}
 
-	return &ninshou.User{Name: in.Name, Email: in.Email}, nil
+	id := [16]byte(uuid.New())
+	user := &ninshou.User{XId: id[:], Name: in.Name, Email: in.Email}
+
+	ins, err := bson.Marshal(user)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	collection := s.Database().Collection("users")
+	log.Debug(user)
+	_, err = collection.InsertOne(ctx, ins)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
