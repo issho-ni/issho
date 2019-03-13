665415bf6b5fb9b07f5b813bef0b1e41a322722b
Jesse B. Hannah <jesse@jbhannah.net>
2019-03-13 14:20:09 -0700

    Index users collection on email field

--- internal/app/ninshou/server.go
+++ internal/app/ninshou/server.go
@@ -1,9 +1,16 @@
 package ninshou
 
 import (
+	"context"
+	"time"
+
 	"github.com/issho-ni/issho/api/ninshou"
 	"github.com/issho-ni/issho/internal/pkg/service"
 
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 	"google.golang.org/grpc"
 )
 
@@ -29,5 +36,24 @@ func (s *ninshouServer) StartServer() {
 	cancel := s.mongoClient.Connect()
 	defer cancel()
 
+	s.createIndexes()
 	s.GRPCServer.StartServer()
+}
+
+func (s *ninshouServer) createIndexes() {
+	log.Debugf("Creating indexes")
+
+	index := mongo.IndexModel{}
+	index.Keys = bsonx.Doc{{Key: "email", Value: bsonx.Int32(1)}}
+	index.Options = options.Index().SetUnique(true)
+
+	createOptions := options.CreateIndexes().SetMaxTime(10*time.Second)
+
+	users := s.mongoClient.Database().Collection("users").Indexes()
+	result, err := users.CreateOne(context.Background(), index, createOptions)
+	if err != nil {
+		log.Fatalf("Could not create index: %v", err)
+	}
+
+	log.Debugf("Created index %s", result)
 }
