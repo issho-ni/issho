9e5e58fee42c63c127eff25a55fe2220101171fa
Jesse B. Hannah <jesse@jbhannah.net>
2019-03-19 13:24:47 -0700

    Add token claim extraction and validation

--- internal/app/ninka/server.go
+++ internal/app/ninka/server.go
@@ -2,11 +2,15 @@ package ninka
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/issho-ni/issho/api/ninka"
 	"github.com/issho-ni/issho/internal/pkg/service"
+	"github.com/issho-ni/issho/internal/pkg/uuid"
 
+	"github.com/pascaldekloe/jwt"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -18,6 +22,13 @@ type ninkaServer struct {
 	service.GRPCServer
 	mongoClient service.MongoClient
 	ninka.NinkaServer
+	secret []byte
+}
+
+// Claims represents a set of claims with the token ID parsed into a UUID.
+type Claims struct {
+	ID uuid.UUID
+	*jwt.Claims
 }
 
 // NewNinkaServer returns a new gRPC server for the Ninka service.
@@ -25,6 +36,7 @@ func NewNinkaServer(config *service.ServerConfig) service.Server {
 	server := &ninkaServer{}
 	server.GRPCServer = service.NewGRPCServer(config, server)
 	server.mongoClient = service.NewMongoClient(config.Name)
+	server.secret = []byte(os.Getenv("NINKA_JWT_SECRET"))
 
 	return server
 }
@@ -62,4 +74,22 @@ func (s *ninkaServer) createIndexes() {
 
 		log.Debugf("Created index %s", result)
 	}
+}
+
+func (s *ninkaServer) extractClaims(token *ninka.Token) (*Claims, error) {
+	t := []byte(token.Token)
+
+	claims, err := jwt.HMACCheck(t, s.secret)
+	if err != nil {
+		return nil, err
+	} else if ok := claims.Valid(time.Now()); !ok {
+		return nil, fmt.Errorf("JWT has expired or contains invalid claims")
+	}
+
+	tokenID, err := uuid.Parse(claims.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Claims{tokenID, claims}, nil
 }
