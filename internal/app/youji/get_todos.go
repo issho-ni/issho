71f1bd56280bf3166b5938c0da65bbc257e082b1
Jesse B. Hannah <jesse@jbhannah.net>
2019-04-05 15:27:34 -0700

    Implement GetTodos

--- internal/app/youji/get_todos.go
+++ internal/app/youji/get_todos.go
@@ -4,8 +4,29 @@ import (
 	"context"
 
 	"github.com/issho-ni/issho/api/youji"
+	icontext "github.com/issho-ni/issho/internal/pkg/context"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (s *youjiServer) GetTodos(ctx context.Context, in *youji.GetTodosParams) (*youji.Todos, error) {
-	panic("Not implemented")
+	results := []*youji.Todo{}
+
+	claims, _ := icontext.ClaimsFromContext(ctx)
+	filter := bson.D{{Key: "userid", Value: claims.UserID}}
+
+	collection := s.mongoClient.Database().Collection("todos")
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	for cur.Next(ctx) {
+		result := &youji.Todo{}
+		if err := cur.Decode(result); err == nil {
+			results = append(results, result)
+		}
+	}
+
+	return &youji.Todos{Todos: results}, nil
 }
--- internal/app/youji/server.go
+++ internal/app/youji/server.go
@@ -1,9 +1,16 @@
 package youji
 
 import (
+	"context"
+	"time"
+
 	"github.com/issho-ni/issho/api/youji"
 	"github.com/issho-ni/issho/internal/pkg/service"
 
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 	"google.golang.org/grpc"
 )
 
@@ -29,5 +36,23 @@ func (s *youjiServer) StartServer() {
 	cancel := s.mongoClient.Connect()
 	defer cancel()
 
+	s.createIndexes()
 	s.GRPCServer.StartServer()
+}
+
+func (s *youjiServer) createIndexes() {
+	log.Debugf("Creating indexes")
+
+	index := mongo.IndexModel{}
+	index.Keys = bsonx.Doc{{Key: "userid", Value: bsonx.Int32(1)}}
+
+	createOptions := options.CreateIndexes().SetMaxTime(10 * time.Second)
+	todos := s.mongoClient.Database().Collection("todos").Indexes()
+
+	results, err := todos.CreateOne(context.Background(), index, createOptions)
+	if err != nil {
+		log.Fatalf("Could not create indexes: %v", err)
+	}
+
+	log.Debugf("Created index %s", results)
 }
