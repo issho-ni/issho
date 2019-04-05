b8cebf4a87d6c890fe771a8eb2d48361c812a973
Jesse B. Hannah <jesse@jbhannah.net>
2019-04-05 15:06:15 -0700

    Directly marshal retrieved credential to struct

--- internal/app/shinninjou/validate_credential.go
+++ internal/app/shinninjou/validate_credential.go
@@ -2,17 +2,15 @@ package shinninjou
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/issho-ni/issho/api/shinninjou"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func (s *shinninjouServer) ValidateCredential(ctx context.Context, in *shinninjou.Credential) (*shinninjou.CredentialResponse, error) {
-	result := &bson.D{}
+	result := &shinninjou.Credential{}
 	filter := bson.D{{Key: "userid", Value: in.UserID}, {Key: "credentialtype", Value: in.CredentialType}}
 
 	collection := s.mongoClient.Database().Collection("credentials")
@@ -21,15 +19,9 @@ func (s *shinninjouServer) ValidateCredential(ctx context.Context, in *shinninjo
 		return nil, err
 	}
 
-	m := result.Map()
-	hash, ok := m["credential"].(primitive.Binary)
-	if !ok {
-		return nil, fmt.Errorf("Could not retrieve bytes of bcrypt hash")
-	}
-
 	switch in.CredentialType {
 	case shinninjou.CredentialType_PASSWORD:
-		err = bcrypt.CompareHashAndPassword(hash.Data, in.Credential)
+		err = bcrypt.CompareHashAndPassword(result.Credential, in.Credential)
 		if err != nil {
 			return nil, err
 		}
