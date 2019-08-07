a800e9a014f62847d7d6bd53e6db97ffa7682632
Jesse B. Hannah <jesse@jbhannah.net>
2019-08-07 13:24:55 -0700

    Use skopeo to make pushable cache image

--- ci/scripts/cache.sh
+++ ci/scripts/cache.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
+set -eu
+
 echo "{\"imageLayoutVersion\":\"1.0.0\"}" > cache/oci-layout
-tar cf cache/image.tar cache/*
+tar cf cache/image-oci.tar cache/*
+/skopeo copy tarball:cache/image-oci.tar docker-archive:cache/image.tar
+rm cache/image-oci.tar
