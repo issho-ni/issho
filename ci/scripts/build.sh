814a1c43dba5cd7fcaf659ae4432bb067ec3e893
Jesse B. Hannah <jesse@jbhannah.net>
2019-08-08 15:45:23 -0700

    Fix build script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,7 +5,7 @@ set -eu
 if [ -n "$DOCKER_HUB_USERNAME" ] && [ -n "$DOCKER_HUB_PASSWORD" ]; then
     auth=$(printf "%s:%s" $DOCKER_HUB_USERNAME $DOCKER_HUB_PASSWORD | base64 | tr -d "\n")
     mkdir ~/.docker
-    echo '{"auths":{"https://index.docker.io/v1/":{"auth":"${auth}"}}}' > ~/.docker/config.json
+    echo "{\"auths\":{\"https://index.docker.io/v1/\":{\"auth\":\"${auth}\"}}}" > ~/.docker/config.json
 fi
 
 exec build
