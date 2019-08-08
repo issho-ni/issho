b10fd02b97d83acaaacf86656e71a97f78f752ce
Jesse B. Hannah <jesse@jbhannah.net>
2019-08-08 11:34:22 -0700

    Fix newline in Docker auth

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-auth=$(printf "%s:%s" "_json_key" "${GCR_JSON_KEY}" | base64)
+auth=$(printf "%s:%s" "_json_key" "${GCR_JSON_KEY}" | base64 | tr -d "\n")
 mkdir ~/.docker
 cat <<EOF > ~/.docker/config.json
 {"auths":{"https://gcr.io/v2/":{"auth":"${auth}"}}}
