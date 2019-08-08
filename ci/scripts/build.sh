92936551bb000f559b5718f40036ddfdd6c5e5ee
Jesse B. Hannah <jesse@jbhannah.net>
2019-08-08 15:24:02 -0700

    Try to only fetch cache once

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-auth=$(printf "%s:%s" $DOCKER_HUB_USERNAME $DOCKER_HUB_PASSWORD | base64 | tr -d "\n")
-mkdir ~/.docker
-cat <<EOF > ~/.docker/config.json
-{"auths":{"https://index.docker.io/v1/":{"auth":"${auth}"}}}
-EOF
+if [ -n "$DOCKER_HUB_USERNAME" ] && [ -n "$DOCKER_HUB_PASSWORD" ]; then
+    auth=$(printf "%s:%s" $DOCKER_HUB_USERNAME $DOCKER_HUB_PASSWORD | base64 | tr -d "\n")
+    mkdir ~/.docker
+    echo '{"auths":{"https://index.docker.io/v1/":{"auth":"${auth}"}}}' > ~/.docker/config.json
+fi
 
-build
+exec build
