adada4b13c3fec92039c1106cc93c578c7b842a6
Jesse B. Hannah <jesse@jbhannah.net>
2019-10-11 14:09:10 -0700

    Use constant strings for GraphQL errors

--- internal/app/graphql/directive.go
+++ internal/app/graphql/directive.go
@@ -9,11 +9,14 @@ import (
 	"github.com/99designs/gqlgen/graphql"
 )
 
+const errAuthenticationRequired = "ERR_AUTHENTICATION_REQUIRED"
+const errAuthenticationForbidden = "ERR_AUTHENTICATION_FORBIDDEN"
+
 func protectedFieldDirective(ctx context.Context, _ interface{}, next graphql.Resolver, authRequired bool) (interface{}, error) {
 	if _, ok := icontext.ClaimsFromContext(ctx); authRequired && !ok {
-		return nil, fmt.Errorf("Authentication required")
+		return nil, fmt.Errorf(errAuthenticationRequired)
 	} else if !authRequired && ok {
-		return nil, fmt.Errorf("Can't do this while logged in")
+		return nil, fmt.Errorf(errAuthenticationForbidden)
 	}
 
 	return next(ctx)
